29bfb860b8ad07340d50bba0755a8d53e3bfb4f6
Dieter Baron <dillo@nih.at>
2020-05-03 11:35:34 +0200

    Add Xcode project for development.
    Assumes libzip is installed in /usr/local.

---
+++ developer-xcode/config.h
@@ -0,0 +1,27 @@
+#ifndef HAD_CONFIG_H
+#define HAD_CONFIG_H
+#define PACKAGE "ziptools"
+#define PACKAGE_BUGREPORT "ziptools@nih.at"
+#define VERSION "0.1"
+
+#define HAVE_FNMATCH
+#define HAVE_GETOPT_LONG
+#define HAVE_GETPROGNAME
+
+#define HAVE_STDBOOL_H
+#define HAVE_STDINT_H
+
+#define HAVE_INT8_T
+#define HAVE_UINT8_T
+#define HAVE_INT16_T
+#define HAVE_UINT16_T
+#define HAVE_INT32_T
+#define HAVE_UINT32_T
+#define HAVE_INT64_T
+#define HAVE_UINT64_T
+#define HAVE_SHORT
+#define HAVE_INT
+#define HAVE_LONG
+#define HAVE_LONG_LONG
+#define HAVE_SIZEOF_OFF_T
+#endif /* HAD_CONFIG_H */
